8a7e225beb0f24afa4a74b9aa6eb8623515a5cd3
gracefullsage <GlorytoAlmightyGod>
2025-01-23 15:22:45 +0530

    C++ upgrade

--- hi.cpp
+++ hi.cpp
@@ -3,6 +3,7 @@
 void create_a_new_file(char name_of_file[]) {
     FILE *fptr;
     fptr = fopen(name_of_file, "w");
+    fclose(fptr);
 }
 
 void write_to_an_existing_file(char name_of_file[], char text_to_be_written[]) {
@@ -10,11 +11,19 @@ void write_to_an_existing_file(char name_of_file[], char text_to_be_written[]) {
     fptr = fopen(name_of_file, "a");
 
     fprintf(fptr, text_to_be_written);
+    fclose(fptr);
+}
+
+void read_an_existing_file(char name_of_file[]) {
+    FILE *fptr;
+    fptr = fopen(name_of_file, "r");
+
+
 }
 
 void test_the_development() {
     for(int i=1;i<=10;i++) {
-        write_to_an_existing_file("auto_generated.c", "message\n");
+        write_to_an_existing_file("auto_generated.cpp", "message\n");
     }
 }
 
