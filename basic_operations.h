38a5fc526dd61adaa206863a63756047a99b1892
gracefullsage <GlorytoAlmightyGod>
2025-04-23 19:48:35 +0530

    added can_do and refactored

---
+++ basic_operations.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+#define MEMORY_POWER 10
+
+string that_which_can_be_done_by_the_self_in_Reality[1+MEMORY_POWER];
+string that_which_is_at_play_for_the_self_in_Reality[1+MEMORY_POWER];
+
+
+void wake_the_data_up(string data[], string data_name) {
+    data[0] = data_name;
+}
+
+void show_the_data(string data[]) {
+    cout << "here is the data : ";
+    for(int i = 0; i <= MEMORY_POWER; i++) {
+        cout << data[i] << endl;
+    }
+}
+
+void collect_the_data(string data[], string filename) {
+    ifstream fptr(filename);
+    for(int index = 1; index <= MEMORY_POWER; index++) {
+        getline(fptr, data[index]);
+    }
+}
+
--- that_which_is_at_play_for_the_self_in_Reality.cpp
+++ that_which_is_at_play_for_the_self_in_Reality.cpp
@@ -1,26 +1,13 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include "basic_operations.h"
 
-using namespace std;
+string find_my_object(string object_to_be_found, string data[]) {
 
-#define MEMORY_POWER 10
-
-string that_which_is_at_play_for_the_self_in_Reality[1+MEMORY_POWER];
-
-string find_my_object(string object_to_be_found, string filename) {
-
-    cout << "searching in : " << filename << endl;
-    
-    ifstream fptr(filename);
-    for(int index = 1; index <= MEMORY_POWER; index++) {
-        getline(fptr, that_which_is_at_play_for_the_self_in_Reality[index]);
-    }
+    cout << "searching for " << object_to_be_found << " in : " << data[0] << endl;
 
     string perception_of_the_object = "FAILED to find the object.";
     
     for (int i = 1; i <= MEMORY_POWER; i++) {
-        if(that_which_is_at_play_for_the_self_in_Reality[i] == object_to_be_found) {
+        if(data[i] == object_to_be_found) {
             perception_of_the_object = "SUCCESS! object encountered.";
             break;
         }
@@ -28,25 +15,15 @@ string find_my_object(string object_to_be_found, string filename) {
     return perception_of_the_object;
 }
 
-void show_the_data(string data[], int length_of_array) {
-    cout << "here is the data : ";
-    for(int i = 0; i < length_of_array; i++) {
-        cout << data[i] << endl;
-    }
-}
-
-void wake_the_data_up() {
-    that_which_is_at_play_for_the_self_in_Reality[0] = "that_which_is_at_play_for_the_self_in_Reality";
-}
-
 int main(void) {
-    wake_the_data_up();
+    wake_the_data_up(that_which_is_at_play_for_the_self_in_Reality, "that_which_is_at_play_for_the_self_in_Reality");
 
     string lost_object;
     cout << "enter the object you want me to find : ";
     cin >> lost_object;
-
-    cout << find_my_object(lost_object, "things_around_me") << endl;
+    
+    collect_the_data(that_which_is_at_play_for_the_self_in_Reality, "things_around_me");
+    cout << find_my_object(lost_object, that_which_is_at_play_for_the_self_in_Reality) << endl;
 
     return 0;
 }
